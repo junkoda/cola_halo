2be7c5d1a0b171cf5ff7c13b6eaa26dc12e9d286
Jun Koda <jun.koda@brera.inaf.it>
2015-10-27 15:41:15 +0100

    Updated mem.rb for fixed FFTW memory allocation. Added subdominant memory usage which are not subdominant for pm_factor= 1.

--- tools/mem.rb
+++ tools/mem.rb
@@ -11,7 +11,8 @@ def mem(nc, nnode, pm_factor= 3)
   part_alloc_factor= 1.5
 
   puts "Memory estimate for nc= #{nc}, #{nnode} nodes" 
-  sizep= 4*3*4 + 8 + 3*4 # particle and force
+  sizep= 4*3*4 + 8 + 3*4   # particle and force
+  sizep_min= 4*(3+3+2)   # position + velocity + id
   mbyte= 1024**2
   gbyte= 1024**3
 
@@ -29,17 +30,33 @@ def mem(nc, nnode, pm_factor= 3)
   end
   puts "PM nx= #{pm_nx}"
 
-  np= part_alloc_factor*nc*nc*(nx+1)
-  mp= part_alloc_factor*nc*nc*(nx+1)*sizep
-  mb= nc*nc*(1)*sizep;
-  mm= (pm_factor*nc)**2*pm_nx*4*2  # factor of 2 for FFTW working memory(?)
-  mm2= (pm_factor*nc)**2*(nx/2+1)*2*4 # if delta_k is copied
+  np= part_alloc_factor*nc*nc*(nx+1)         # number of particles per node
+  mp= part_alloc_factor*nc*nc*(nx+1)*sizep   # RAM for particles
+  mb= nc*nc*(1)*sizep;                       # particle communication buffer
+  mm= (pm_factor*nc)**2*pm_nx*4              # PM mesh density -> force
+  mm2= (pm_factor*nc)**2*(nx/2+1)*2*4        # PM mesh for density
 
   printf("%.1f Mbytes for %d particle\n", mp/mbyte, np)
   printf("%.1f Mbytes for communication buffer\n", mb/mbyte)
-  printf("%.1f Mbytes for one PM mesh\n", mm/mbyte)
+  printf("%.1f Mbytes for one PM mesh (mem1)\n", mm/mbyte)
 
-  printf("%.3f Gbytes per core in total\n", (mp + mb + mm + mm2)/gbyte)
+  # memory for LPT
+  m_lpt= nc**2*nx*4*12
+  printf("%.1f Mbytes for 2LPT (mem1)\n", m_lpt/mbyte)
+  
+  # memory for snapshot
+  m_snp= part_alloc_factor*nc*nc*(nx+1)*sizep_min
+
+  printf("%.1f Mbytes for snapshot (mem2)\n", m_snp/mbyte)
+
+  # memory shared for PM 2LPT/Particle Mesh/FoF halo finder/snapshot
+  mem1= [m_lpt, mm].max
+  mem2= [m_snp, mm2].max
+
+  printf("%.1f Mbytes for mem1 (PM/LPT)\n", mem1/mbyte)
+  printf("%.1f Mbytes for mem2 (PM/snapshot)\n", mem2/mbyte)
+
+  printf("%.3f Gbytes per core in total (particle+buffer+mem1+mem2)\n", (mp + mb + mm + mm2)/gbyte)
   #printf("%.3f Gbyte for particle output\n", nc**3.0*32/gbyte) # long id
   printf("\n")
 end
